86c5dcc4a77ab46172eb13fb914e022b162137a9
NaokiIto0719 <tihynhts_ntbm@yahoo.co.jp>
2024-05-28 14:33:15 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -32,9 +32,11 @@ def communicate():
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
-st.write("----- プロンプト ---------------")
 st.write("--------------------------------")
+st.write("-")
+st.write("#プロンプト#")
 st.write(st.secrets.AppSettings.chatbot_setting)
+st.write("--------------------------------")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
